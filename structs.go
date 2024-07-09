e6ef5b042d92ffa7a599bdd27555f3011edbaa6a
Chidinma Ehirim <123429237+chiaxx@users.noreply.github.com>
2024-07-08 21:16:51 -0600

    Update structs.go

--- structs.go
+++ structs.go
@@ -19,4 +19,13 @@ func main(){
 	}
 	fmt.Println("My favourite fruits is a ", m.name, "and its color is " , m.color,  "and i currently have ",m.quantity, "in my fridge")
 
+	//with a dot notation 
+	// var h Fruit
+	// 	h.name = "pineapple"
+	// 	h.color = "yellow"
+	// 	h.quantity = 21
+	
+	// fmt.Println("My favourite fruits is a ", h.name, "and its color is " , h.color,  "and i currently have ",h.quantity, "in my fridge")
+
+
 }
